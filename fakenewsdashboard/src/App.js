c2c51e8343cb49297a4b863f5c1d9908374216b5
CiTRuS93 <hadarradah@gmail.com>
2021-01-03 13:04:55 +0200

    create react

---
+++ fakenewsdashboard/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Temperature from './component/dashboard/temperature.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Temperature />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ fakenewsdashboard/src/component/dashboard/temperature.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import Thermometer from 'react-thermometer-component'
+import axios from "axios"; 
+
+function Temperature() {
+  const [sentiment, setSentiment] = useState(0);
+
+  const [fakiness, setFakiness] = useState(0);
+
+  const [authenticity, setAuthenticity] = useState(0);
+
+  async function getTemp() {
+    axios
+      .get("/getTemp")
+      .then((res) => {
+        setSentiment(res.data.sentiment);
+        setFakiness(res.data.fakiness);
+        setAuthenticity(res.data.authenticity);
+      })
+      .catch(() => {
+        setSentiment(14);
+        setFakiness(0);
+        setAuthenticity(2);
+      });
+  }
+  useEffect(() => {
+    getTemp();
+  }, []);
+
+  return (
+    <div className="Widget">
+        <Thermometer
+          theme="dark"
+          value={sentiment}
+          max="100"
+          steps="3"
+          format="%"
+          size="large"
+          height="300"
+        /><h3>sentiment</h3>
+        <Thermometer
+          theme="dark"
+          value={fakiness}
+          max="100"
+          steps="3"
+          format="%"
+          size="large"
+          height="300"
+        /><h3>fakiness</h3>
+        <Thermometer
+          theme="dark"
+          value={authenticity}
+          max="100"
+          steps="3"
+          format="%"
+          size="large"
+          height="300"
+        /><h3>authenticity</h3>
+    </div>
+  );
+}
+
+export default Temperature;
